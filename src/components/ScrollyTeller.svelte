38ae4e3748e8d5fc8664f2265e7671873dcd07ee
m1nce <miinchaankiim@gmail.com>
2024-03-04 13:55:44 -0800

    detached Graph.svelte

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -96,7 +96,7 @@
     <Map {index}/>
     <F1 {index}/>
     <TextBase {index}/>
-    <Graph {index}/>
+    <!-- <Graph {index}/> -->
 
     <div class='progress-bars'>
       <p>current section: <strong>{index + 1}/{count}</strong></p>
