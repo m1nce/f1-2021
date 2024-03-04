4649bba467f522f738e0d5e90c293a915a1c0423
m1nce <miinchaankiim@gmail.com>
2024-03-04 13:31:05 -0800

    added script to get telemetry data

---
+++ get_data.py
@@ -0,0 +1,15 @@
+# Necessary Packages
+import pandas as pd
+import fastf1
+
+# Load the 2021 Abu Dhabi Race
+session = fastf1.get_session(2021, 'abudabi', 'Race')
+session.load(weather=False, messages=False)
+
+# Get the last lap data for position
+ver_last_lap = session.laps.pick_lap(58).pick_driver('VER').get_pos_data()
+ham_last_lap = session.laps.pick_lap(58).pick_driver('HAM').get_pos_data()
+
+# Convert DataFrames into json to use
+ver_last_lap.to_json('./static/ver_tele')
+ham_last_lap.to_json('./static/ham_tele')
